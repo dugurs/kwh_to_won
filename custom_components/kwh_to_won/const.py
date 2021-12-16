e908dc6e2fbde37a87e8463e930cfba9a767e237
dugurs <dugurs@gmail.com>
2021-12-16 19:18:38 +0900

    ..

--- custom_components/kwh_to_won/const.py
+++ custom_components/kwh_to_won/const.py
@@ -3,7 +3,7 @@
 # This is the internal name of the integration, it should also match the directory
 # name for the integration.
 DOMAIN = "kwh_to_won"
-VERSION = "0.0.4"
+VERSION = "0.0.5"
 MODEL = "kwh2won"
 MANUFACTURER = ""
 
