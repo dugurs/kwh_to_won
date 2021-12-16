bc340525a3ef1bb1a37e89cf633b18650ccf5663
dugurs <dugurs@gmail.com>
2021-12-16 18:10:43 +0900

    단가 파리미터 삭제(이동)

--- custom_components/kwh_to_won/const.py
+++ custom_components/kwh_to_won/const.py
@@ -3,7 +3,7 @@
 # This is the internal name of the integration, it should also match the directory
 # name for the integration.
 DOMAIN = "kwh_to_won"
-VERSION = "0.0.3"
+VERSION = "0.0.4"
 MODEL = "kwh2won"
 MANUFACTURER = ""
 
@@ -24,28 +24,4 @@ WELFARE_DC_OPTION = {
     3:'기초생활(생계.의료)',
     4:'기초생활(주거.교육)',
     5:'차사위계층'
-}
-
-# https://cyber.kepco.co.kr/ckepco/front/jsp/CY/E/E/CYEEHP00101.jsp
-CALC_PARAMETER = {
-    'low': {
-        'basicprice' : [910, 1600, 7300], # 기본요금(원/호)
-        'kwhprice' : [88.3, 182.9, 275.6], # 전력량 요금(원/kWh)
-        # 'kwhprice' : [93.3, 187.9, 280.6], # 전력량 요금(원/kWh) - 개편전 요금
-        'kwhsectionUp' : [0, 200, 400], # 구간(kWh - 상계)
-        'kwhsectionDown' : [0, 300, 450], # 구간(kWh - 하계)(7~8월)
-        'adjustment' : [-5, 5.3, 0], # 환경비용차감 + 기후환경요금 + 연료비조정액
-        # 'adjustment' : [-5, 5.3, -3], # 환경비용차감 + 기후환경요금 + 연료비조정액 - 개편전 요금
-        'elecBasicLimit' : 2000,
-        'elecBasic200Limit' : 4000
-    },
-    'high': {
-        'basicprice' : [730, 1260, 6060], # 기본요금(원/호)
-        'kwhprice' : [73.3, 142.3, 210.6], # 전력량 요금(원/kWh)
-        'kwhsectionUp' : [0, 200, 400], # 구간(kWh - 상계)
-        'kwhsectionDown' : [0, 300, 450], # 구간(kWh - 하계)(7~8월)
-        'adjustment' : [-5, 5.3, 0], # 환경비용차감 + 기후환경요금 + 연료비조정액
-        'elecBasicLimit' : 1500,
-        'elecBasic200Limit' : 2500
-    },
 }
